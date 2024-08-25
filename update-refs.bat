fbf0d6af83120a38bcdfcdc0c4d0fbd5fe8b812b
Leo <leo@nullptr.com.cn>
2024-08-25 17:43:05 +0800

    add 3rdparty refs and update script

---
+++ update-refs.bat
@@ -0,0 +1,2 @@
+curl -o 3rdparty/AIsouler_gkd.json -L https://registry.npmmirror.com/@aisouler/gkd_subscription/latest/files/dist/AIsouler_gkd.json5
+curl -o 3rdparty/Adpro_gkd.json -L https://registry.npmmirror.com/@adpro/gkd_subscription/latest/files/dist/Adpro_gkd.json5
