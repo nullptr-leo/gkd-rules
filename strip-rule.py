7f5b274e3f2dd0ea850b8cb66c4009a8c28a6acb
Leo <leo@nullptr.com.cn>
2024-08-25 22:03:11 +0800

    support generate package query commands

--- strip-rule.py
+++ strip-rule.py
@@ -2,16 +2,20 @@ import execjs
 import json
 from openpyxl import load_workbook
 
-rule_path = 'rule.json'
-rule_strip_path = 'rule-strip.json'
+rule_path = 'rules/rule.json'
+rule_strip_path = 'rules/rule-strip.json'
+extra_cmd_path = 'extra-cmd.bat'
 
 # load pkg defs
 strip_list = [ ]
+allow_list = [ ]
 workbook = load_workbook('pkg-info.xlsx')
 worksheet = workbook.active
 for row in worksheet.iter_rows(min_row=1, max_col=3, values_only=True):
     if row[2] == 1:
         strip_list.append(row[0])
+    elif row[2] == 0:
+        allow_list.append(row[0])
 
 with open(rule_path, 'r', encoding='utf-8') as file:
     rule = execjs.eval(file.read())
@@ -21,3 +25,9 @@ with open(rule_path, 'r', encoding='utf-8') as file:
 
     with open(rule_strip_path, 'w', encoding='utf-8') as strip_file:
         json.dump(rule, strip_file, indent=4, ensure_ascii=False)
+
+    with open(extra_cmd_path, 'w', encoding='utf-8') as extra_file:
+        for app in app_list_strip:
+            if app['id'] in allow_list:
+                continue
+            extra_file.write(f'query-pkg-info.py {app["id"]}\n')
